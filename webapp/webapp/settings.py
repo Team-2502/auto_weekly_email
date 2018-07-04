9a5fbc5eb40242d173a9520d771be9fa570330ed
Ritik Mishra <ritik.mishra314@gmail.com>
2018-07-04 01:40:29 -0500

    allowed hosts is still broken

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "HEROKU" not in os.environ.keys()
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
