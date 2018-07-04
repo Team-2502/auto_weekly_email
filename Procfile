da1be849e50a08b1713183b4c1803e9e19d3c54a
Ritik Mishra <ritik.mishra314@gmail.com>
2018-07-04 01:35:34 -0500

    Fix settings, procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python webapp/manage.py runserver
+web: python webapp/manage.py runserver 0.0.0.0:$PORT
--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "HEROKU" in os.environ.keys()
+DEBUG = "HEROKU" not in os.environ.keys()
 
 ALLOWED_HOSTS = []
 
